b246af3e7b3290fb67138251e2f33358fb27c3bc
camilpz <camilaal895@gmail.com>
2025-04-18 22:19:41 -0300

    Basic structure

---
+++ users/src/test/java/com/inf/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
